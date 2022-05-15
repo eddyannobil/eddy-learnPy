c64821702c43cf67cfabf3321abbc8c7c832baf8
Eddy Annobil <edmundannobil@gmail.com>
2022-05-15 01:58:21 -0400

    Initial commit

---
+++ helping_sally.py
@@ -0,0 +1,71 @@
+```
+business-name = 'Sally's Fruity Loops'
+
+This is the list of all the fruits I'll be selling
+
+fruit-list = ["Apple", "Banana", "Cherries", "Dragon Fruit" Fig
+               Grapefruit "Kiwi", "Lemon" Lime" Mango" "Oranges", "Pineapple
+              "Grapes" Watermelon".]
+
+This is the list of the prices of each fruit in the fruit list.
+
+fruit_prices-list = [ 1.35, 1.30 0.32 0.60 0.80 1.76 3.32 0.34 2 3 2.46 1.43
+                      1.8 1.9.]
+
+Check if the first item in the fruit list is apple and then display
+a notice to customers.
+
+if fruit-list[0] == "apple":
+print("Sally sells apples too!").
+
+
+Calculate how much discount to give customers on opening day
+by adding the prices of the first 2 fruits, divide by 100 and
+multiply by 5
+
+discount-amount = (1.35 + 1.30 / 100) * 5
+discount-percent = discount-price
+print("Thanks for being a valued customer! Your discount is: ", discount-percent, "%")
+
+'' The code below will be used to show customers a list of random fruits on sale. ''
+
+first sample from fruit-list = fruit-list[1]
+second sample from fruit-list = fruit-list[13]
+third sample from fruit-list = fruit-list[9]
+fourth sample from fruit-list = fruit-list[17]
+fifth sample from fruit-list = fruit-list[1]
+
+print("Here are the fruits on sale today:")
+print(first sample from fruit-list)
+print(second sample from fruit-lists)
+print(third samples from fruit-list)
+print(fourth sample from fruit-lists)
+
+This is a list of the first three fruits. I'll give these for free on my birthday
+
+1fruit, 2fruit, 3fruit = fruit-list[1],fruit-list[2],fruit-list[3]
+print(1fruit, 2fruit, 3fruit).
+
+Declare a variable to store a customer's shopping cart total BEFORE they start shopping'
+
+customer-total = 
+
+This is the raw string that will be displayed on the homepage of the store's website
+The website can process newline characters (\n) and turn them into smiley faces,
+so we'll need those to show when the string is printed
+
+Raw_Welcome = "Welcome to Sally's \n fruity loop store"
+print(raw_welcome)
+
+Check that the customer cart total works properly by testing the cart total functionality
+
+customer-total = customer-total + 10
+print(customer-total).
+
+Declare a string to thank a customer for shopping and show them their cart total
+after ordering:
+
+goodbye_msg = "Thanks for shopping at Sally's Fruity Loops'
+print("Your total for today is: Customer-Total")
+
+```
